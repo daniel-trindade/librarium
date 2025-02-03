5c86e7a7b63ac2e665a2835f833fb6d642621acd
root <danielbrunotrindade@gmail.com>
2025-02-03 18:58:39 -0300

    Footer finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentContainerComponent } from './components/content-container/content-container.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [
+    NavBarComponent,
+    FooterComponent,
+    ContentContainerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
